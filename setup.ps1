6c57b5676583c8ff4f9759a88cce6017389f2809
Chaitanya Sharma <7569010+chaitanyakush@users.noreply.github.com>
2022-02-27 19:05:09 +0530

    Create setup.ps1

---
+++ setup.ps1
@@ -0,0 +1,11 @@
+iwr -useb get.scoop.sh | iex
+scoop install curl sed jq wget bat git
+mkdir $env:USERPROFILE/.config/powershell
+cp user_profile.ps1 $env:USERPROFILE/.config/powershell/
+Install-Module posh-git -Scope CurrentUser -Force
+Install-Module oh-my-posh -Scope CurrentUser -Force
+cp chaitanya.omp.json $env:USERPROFILE/.config/powershell/
+echo '. $env:USERPROFILE\.config\powershell\user_profile.ps1' > $PROFILE.CurrentUserCurrentHost
+Install-Module Terminal-Icons -Repository PSGallery -Force
+Install-Module -Name z -Force
+Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
